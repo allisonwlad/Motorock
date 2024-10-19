165baf14ca402753895c2105d48b895601a9c0b4
Allison Wladmir <allisonwlad@gmail.com>
2024-10-19 12:34:50 -0300

    upload file process

--- MotorockWeb/src/pages/inicio.py
+++ MotorockWeb/src/pages/inicio.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import requests
 
 st.markdown("""
     <style>
@@ -13,4 +13,10 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.write("voila")
+url = "http://localhost:8081/v1/fileProcess/upload"
+
+uploaded_pdf = st.file_uploader('Upload a file', type="csv")
+if st.button("Upload CSV"):
+    files = {'file': (uploaded_pdf)}
+    response = requests.post(url, files=files)
+    st.write(response)
